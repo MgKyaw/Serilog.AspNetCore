f091e8385d658947295ad3abfb78a02e4630e90c
MgKyaw <mgkyaw20112@gmail.com>
2025-08-21 23:01:14 +0700

    feat: Update Program.cs to config Serilog

--- Serilog.AspNetCore/Program.cs
+++ Serilog.AspNetCore/Program.cs
@@ -1,25 +1,34 @@
-var builder = WebApplication.CreateBuilder(args);
+using Serilog;
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration()
+.WriteTo.Console()
+.CreateLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Information("Starting web application");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Fix: Use AddSerilog and configure Serilog explicitly    
+    builder.Host.UseSerilog((context, services, configuration) =>
+    {
+        // Fix: Ensure Serilog.Settings.Configuration package is installed  
+        configuration.ReadFrom.Configuration(context.Configuration)
+                     .ReadFrom.Services(services)
+                     .WriteTo.Console();
+    });
+
+    var app = builder.Build();
+    app.MapGet("/", () => "Hello World!");
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
