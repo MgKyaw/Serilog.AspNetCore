92e1e7cc97b18af01064a543de4d6c8f383c11c1
MgKyaw <mgkyaw20112@gmail.com>
2025-08-22 21:44:33 +0700

    feat: Use builder.Services.AddSerilog instead of builder.Host.UseSerilog

--- Serilog.AspNetCore/Program.cs
+++ Serilog.AspNetCore/Program.cs
@@ -13,14 +13,20 @@ try
 
     var builder = WebApplication.CreateBuilder(args);
 
-    // Fix: Use AddSerilog and configure Serilog explicitly    
-    builder.Host.UseSerilog((context, services, configuration) =>
-    {
-        // Fix: Ensure Serilog.Settings.Configuration package is installed  
-        configuration.ReadFrom.Configuration(context.Configuration)
-                     .ReadFrom.Services(services)
-                     .WriteTo.Console();
-    });
+    //// Fix: Use AddSerilog and configure Serilog explicitly    
+    //builder.Host.UseSerilog((context, services, configuration) =>
+    //{
+    //    // Fix: Ensure Serilog.Settings.Configuration package is installed  
+    //    configuration.ReadFrom.Configuration(context.Configuration)
+    //                 .ReadFrom.Services(services)
+    //                 .WriteTo.Console();
+    //});
+
+    builder.Services.AddSerilog((services, lc) => lc
+    .ReadFrom.Configuration(builder.Configuration)
+    .ReadFrom.Services(services)
+    .Enrich.FromLogContext()
+    .WriteTo.Console());
 
     var app = builder.Build();
 
