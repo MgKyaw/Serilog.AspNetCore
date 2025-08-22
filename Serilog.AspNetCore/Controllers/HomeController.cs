7844e107b31e7868ab90d45de4d4804fade798c2
MgKyaw <mgkyaw20112@gmail.com>
2025-08-22 21:17:35 +0700

    feat: Modify HomeController

--- Serilog.AspNetCore/Controllers/HomeController.cs
+++ Serilog.AspNetCore/Controllers/HomeController.cs
@@ -3,8 +3,19 @@
 namespace Serilog.AspNetCore.Controllers;
 public class HomeController : Controller
 {
+    readonly IDiagnosticContext _diagnosticContext;
+
+    public HomeController(IDiagnosticContext diagnosticContext)
+    {
+        _diagnosticContext = diagnosticContext ??
+            throw new ArgumentNullException(nameof(diagnosticContext));
+    }
+
     public IActionResult Index()
     {
+        // The request completion event will carry this property
+        _diagnosticContext.Set("CatalogLoadTime", 1423);
+
         return View();
     }
 }
